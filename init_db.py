a281c10805fc35d700bdc8deb1b0b04a58b1bcbd
Ashvin Nair <nair.ashvin@gmail.com>
2013-09-28 14:08:55 -0700

    Added ids to database object

--- init_db.py
+++ init_db.py
@@ -1,8 +1,9 @@
 import sqlite3
 conn = sqlite3.connect('singleminded.db')
 c = conn.cursor()
-c.execute("CREATE TABLE tasks (title, description, timestamp, deadline, priority, effort, ordinality)")
-c.execute("INSERT INTO tasks VALUES ('buy milk', '', '9-28-13', '9-29-13', 1, 2, 0)")
+c.execute("DROP TABLE IF EXISTS tasks")
+c.execute("CREATE TABLE tasks (taskid, userid, title, description, timestamp, deadline, priority, effort, ordinality)")
+c.execute("INSERT INTO tasks VALUES (0, 0, 'buy milk', '', '9-28-13', '9-29-13', 1, 2, 0)")
 c.execute("SELECT * FROM tasks")
 print(c.fetchall())
 conn.commit()
